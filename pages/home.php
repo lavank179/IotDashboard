2eb6895fe4e0fd361e29d0889edb92dcf7aef7f1
lavank179 <chlavankumar179@gmail.com>
2021-03-08 20:15:49 +0530

    redefined layout/structure2

---
+++ pages/home.php
@@ -0,0 +1,48 @@
+<div class="card">
+    <div class="card-content">
+        <div class="card-title" style="text-align: center;">
+            <h2>IoT Dashboard</h2>
+
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-auto col-md-auto col-lg-auto">
+            <div class="card text-center py-5">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <label class="switch">
+                            <input class="onoff but" type="checkbox" checked id="111">
+                            <span class="round"> </span>
+                        </label>
+                    </div>
+                    <div class="col-sm-4">
+                        <h4> Led 1 </h4>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-4">
+                        <label class="switch">
+                            <input class="onoff but" type="checkbox" checked id="112">
+                            <span class="round"> </span>
+                        </label>
+                    </div>
+                    <div class="col-sm-4">
+                        <h4> Led 2 </h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-auto col-md-auto col-lg-auto">
+            <div class="card">
+                <div class="card-content">
+                    <div id="chart1">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+</div>
