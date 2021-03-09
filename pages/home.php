f7786f34696c8dccd77e2dc882e976781036f7cc
lavank179 <chlavankumar179@gmail.com>
2021-03-09 20:24:33 +0530

    pages, siders, logs...2

--- pages/home.php
+++ pages/home.php
@@ -8,30 +8,40 @@
 
     <div class="row">
         <div class="col-sm-4 col-md-4 col-lg-4">
-            <div class="card text-center py-5">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <label class="switch">
-                            <input class="onoff but" type="checkbox" checked id="111">
-                            <span class="round"> </span>
-                        </label>
-                    </div>
-                    <div class="col-sm-6">
-                        <h4> Led 1 </h4>
-                    </div>
-                </div>
+            <div class="card text-center py-5" id="ledCollection">
+                <table class="table table-bordered">
 
-                <div class="row">
-                    <div class="col-sm-6">
-                        <label class="switch">
-                            <input class="onoff but" type="checkbox" checked id="112">
-                            <span class="round"> </span>
-                        </label>
-                    </div>
-                    <div class="col-sm-6">
-                        <h4> Led 2 </h4>
-                    </div>
-                </div>
+
+                    <thead>
+                        <tr>
+                            <th scope="col"> </th>
+                            <th scope="col">
+                                <h3> Lights & Fans </h3>
+                            </th>
+
+                            <th scope="col">
+                                <!-- Button trigger modal -->
+                                <a type="button" class="btn btn-light btn-sm" data-mdb-toggle="modal" data-mdb-target="#exampleModal">
+                                    <i class="fas fa-plus-square fa-3x"></i></a>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>
+                                <h4> Led 1 </h4>
+                            </td>
+                            <td>
+                                <label class="switch">
+                                    <input class="onoff but" type="checkbox" checked id="111">
+                                    <span class="round"> </span>
+                                </label>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
             </div>
             <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content</p>
         </div>
