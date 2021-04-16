031dfbe509c7bdd4c23c029f1ce21c1ba2c6c069
lavank179 <chlavankumar179@gmail.com>
2021-04-16 20:00:18 +0530

    auth JS pages added

---
+++ js/authUI/login.js
@@ -0,0 +1,50 @@
+let v1 = false,
+  v2 = false;
+EDsubbtn();
+
+function getInputVals(a1, a2, a3, a4) {
+  var text = document.querySelector("form #" + a1).value;
+  if (!a2.test(text)) {
+    $("#" + a4).html(a3);
+    $("#" + a4).addClass("alert-danger");
+    $("#" + a1).addClass("addRed");
+    $("#" + a1).removeClass("addGreen");
+    if (a1 == "email") v1 = false;
+    if (a1 == "password") v2 = false;
+  } else {
+    $("#" + a4).removeClass("alert-danger");
+    $("#" + a4).empty();
+    $("#" + a1).addClass("addGreen");
+    $("#" + a1).removeClass("addRed");
+    if (a1 == "email") v1 = true;
+    if (a1 == "password") v2 = true;
+  }
+  EDsubbtn();
+}
+$("form #email").keyup(function () {
+  var regx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  getInputVals(
+    "email",
+    regx,
+    "Email should be valid. EG: example@example.com",
+    "er1"
+  );
+});
+
+$("form #password").keyup(function () {
+  var regx = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+  getInputVals(
+    "password",
+    regx,
+    "Password shuld be [7 to 15 characters which contain at least one numeric digit and a special character].",
+    "er2"
+  );
+});
+
+function EDsubbtn() {
+  if (v1 == true && v2 == true) {
+    document.querySelector("#LoginSub").disabled = false;
+  } else {
+    document.querySelector("#LoginSub").disabled = true;
+  }
+}
